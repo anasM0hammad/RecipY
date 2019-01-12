1840954e5aae7e6b088c889fb5911b04e178cc73
anasM0hammad <anas.1633.m@gmail.com>
2019-01-13 02:34:36 +0530

    recipe item comp connect with recipe comp

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { Recipe } from '../recipe.model' ;
 
 @Component({
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model' ;
 })
 export class RecipeListComponent implements OnInit {
 
-	recipes: Recipe[] = [
+	 recipes: Recipe[] = [
 
     new Recipe('Pastry' , 'Unique Apple Cream Pastry' , 'http://www.myiconfinder.com/uploads/iconsets/256-256-7275aebc435153f103fe46c706a7f332-pastry.png'),
     new Recipe('Chiken Leg' , 'All time Favorite' , 'https://cdn0.iconfinder.com/data/icons/cartoon-food/512/Food_512-22.png')
