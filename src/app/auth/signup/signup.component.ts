d023704da089215cad5da12f94d8fe417677f5e3
anasM0hammad <anas.1633.m@gmail.com>
2019-01-26 16:07:51 +0530

    validation of signup form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms' ;
 
 @Component({
   selector: 'app-signup',
@@ -11,5 +12,9 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
   }
+
+   onSubmit(form: NgForm){
+    	console.log(form);
+    }
 
 }
