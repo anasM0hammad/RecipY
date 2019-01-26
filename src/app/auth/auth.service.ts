07713189c7cab4329faff4698808fc896ee99287
anasM0hammad <anas.1633.m@gmail.com>
2019-01-26 23:16:46 +0530

    updated header depending upon auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -28,4 +28,8 @@ export class AuthService{
     	return this.token ;
     }
 
+    isAuthenticated(){
+    	return this.token != null ;
+    }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component , OnInit } from '@angular/core' ;
 import { DataStorageService } from '../shared/data-storage.service' ;
 import { Subscription } from 'rxjs' ;
+import { AuthService } from '../auth/auth.service' ;
 
 @Component({
  selector : 'app-header',
@@ -16,7 +17,7 @@ export class headerComponent implements OnInit{
 // 	}
 
 
- constructor(private dataStorage: DataStorageService) {}
+ constructor(private dataStorage: DataStorageService , private authService: AuthService) {}
 
  saveData(){
    this.dataStorage.storeRecipes().subscribe(
